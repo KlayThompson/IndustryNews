087e8287d83bb1b2c3faa9572ea02d27f324a66d
liufeng <liufeng@9tongmail.com>
2016-08-24 14:17:42 +0800

    no message

---
+++ IndusturyNews/IndusturyNews/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  IndusturyNews
+//
+//  Created by liufeng on 16/8/24.
+//  Copyright © 2016年 liufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
