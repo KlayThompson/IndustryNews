7883b1f0a62516b2a114dae2e019e18d5e7ded9c
KlayThompson <913661647@qq.com>
2016-08-31 23:33:26 +0800

    nomessage

---
+++ BusinessNews/BusinessNews/Architecture/Categorys/UIView/UIView+Size.h
@@ -0,0 +1,135 @@
+//  Copyright (c) 2011, Kevin O'Neill
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//
+//  * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+//
+//  * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+//  * Neither the name UsefulBits nor the names of its contributors may be used
+//   to endorse or promote products derived from this software without specific
+//   prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Size)
+
+@property (nonatomic, assign) CGSize size;
+
+/**
+ * Shortcut for frame.origin.x.
+ *
+ * Sets frame.origin.x = left
+ */
+@property (nonatomic, assign) CGFloat left;
+@property (nonatomic, assign) CGFloat x;
+
+/**
+ * Shortcut for frame.origin.x + frame.size.width
+ *
+ * Sets frame.origin.x = right - frame.size.width
+ */
+@property (nonatomic, assign) CGFloat right;
+
+/**
+ * Shortcut for frame.origin.y
+ *
+ * Sets frame.origin.y = top
+ */
+@property (nonatomic, assign) CGFloat top;
+@property (nonatomic, assign) CGFloat y;
+
+/**
+ * Shortcut for frame.origin.y + frame.size.height
+ *
+ * Sets frame.origin.y = bottom - frame.size.height
+ */
+@property (nonatomic, assign) CGFloat bottom;
+
+/**
+ * Shortcut for center.x
+ *
+ * Sets center.x = centerX
+ */
+@property (nonatomic, assign) CGFloat centerX;
+
+/**
+ * Shortcut for center.y
+ *
+ * Sets center.y = centerY
+ */
+@property (nonatomic, assign) CGFloat centerY;
+
+/**
+ * Shortcut for frame.size.width
+ *
+ * Sets frame.size.width = width
+ */
+@property (nonatomic, assign) CGFloat width;
+
+/**
+ * Shortcut for frame.size.height
+ *
+ * Sets frame.size.height = height
+ */
+@property (nonatomic, assign) CGFloat height;
+
+
+/**
+ * Shortcut for frame.origin
+ *
+ * Sets frame.origin = origin
+ */
+@property (nonatomic, assign) CGPoint origin;
+
+/**
+ * Return the x coordinate on the screen.
+ */
+@property (nonatomic, readonly) CGFloat ttScreenX;
+
+/**
+ * Return the y coordinate on the screen.
+ */
+@property (nonatomic, readonly) CGFloat ttScreenY;
+
+/**
+ * Return the x coordinate on the screen, taking into account scroll views.
+ */
+@property (nonatomic, readonly) CGFloat screenViewX;
+
+/**
+ * Return the y coordinate on the screen, taking into account scroll views.
+ */
+@property (nonatomic, readonly) CGFloat screenViewY;
+
+/**
+ * Return the view frame on the screen, taking into account scroll views.
+ */
+@property (nonatomic, readonly) CGRect screenFrame;
+
+@property (nonatomic, readonly) CGRect frameInWindow;
+
+/**
+ * Calculates the offset of this view from another view in screen coordinates.
+ *
+ * otherView should be a parent view of this view.
+ */
+- (CGPoint)offsetFromView:(UIView*)otherView;
+
+@end
--- BusinessNews/BusinessNews/MainViewController/Home/Controllers/HomeViewController.m
+++ BusinessNews/BusinessNews/MainViewController/Home/Controllers/HomeViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "HomeViewController.h"
+#import "NinaPagerView.h"
+#import "Masonry.h"
+#import "MyCollectionViewController.h"
+#import "SelectIndustryTagView.h"
+#import "UIView+Size.h"
 
 @interface HomeViewController ()
+
+@property (nonatomic, strong) SelectIndustryTagView *tagView;
 
 @end
 
@@ -18,14 +25,61 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.view.backgroundColor = COLOR_UI_BG;
+    self.navigationController.navigationBar.translucent = NO;
     
-    self.title = @"商业头条";
-    
+    [self setUpSubViews];
     
 }
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+- (void)setUpSubViews {
+
+    NSArray *titleArray = @[
+                            @"全部",
+                            @"服装服饰",
+                            @"微商",
+                            @"汽车配件",
+                            @"新浪新闻",
+                            @"电子设备",
+                            @"互联网",
+                            @"金融投资",
+                            @"美发造型"
+                            ];
+    NSMutableArray *vcsArray = [NSMutableArray new];
+    
+    for (int index = 0; index < titleArray.count; index ++) {
+        
+        MyCollectionViewController *view = [[MyCollectionViewController alloc] init];
+        
+        [vcsArray addObject:view];
+    }
+    
+    NSArray *colorArray = @[
+                            kColorWithHex(0xd41e12), /**< Title SelectColor  **/
+                            kColorWithHex(0x7b7b7b), /**< Title UnselectColor **/
+                            [UIColor redColor], /**< Underline or SlideBlock Color   **/
+                            [UIColor whiteColor], /**< TopTab Background Color   **/
+                            ];
+    
+    NinaPagerView *test = [[NinaPagerView alloc] initWithNinaPagerStyle:NinaPagerStyleStateNormal WithTitles:titleArray WithVCs:vcsArray WithColorArrays:colorArray];
+    //    test.frame = CGRectMake(0, 0, WIDTH_SCREEN, 44);
+    [self.view addSubview:test];
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"covered_bg"]];
+    imageView.frame = CGRectMake(WIDTH_SCREEN - 60, 0, 60, 34);
+    [test addSubview:imageView];
+    
+    //添加一个button展开tag
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(WIDTH_SCREEN - 30, 13, 14, 16);
+    [button addTarget:self action:@selector(showTagViewButtonClick) forControlEvents:UIControlEventTouchUpInside];
+    [button setImage:[UIImage imageNamed:@"drop-down"] forState:UIControlStateNormal];
+    [test addSubview:button];
+}
+
+#pragma mark - Actions
+- (void)showTagViewButtonClick {
+    
 }
 
 - (NSString *)title {
